61f4fa2ee61633e96705964135dabe9cb6ec54c9
ItemZheng <3160102332@zju.edu.cn>
2019-10-29 16:25:49 +0800

    fixbug: continue while JD http error and early stop

--- comments_JD.py
+++ comments_JD.py
@@ -6,13 +6,13 @@ import codecs
 from tqdm import *
 
 # 原网页
-origin_page = "https://item.jd.com/3604840.html"
+origin_page = "https://item.jd.com/5177847.html"
 
 # 请求参数
 ip = "https://sclub.jd.com/comment/productPageComments.action"
 query_args = {
     "callback": "fetchJSON_comment98vv2645",
-    "productId": "3604840",
+    "productId": "5177847",
     "score": "0",
     "sortType": "5",
     "page": "0",
@@ -22,7 +22,7 @@ query_args = {
     "fold": 0
 }
 request_headers = {
-    "Referer": "https://item.jd.com/3604840.html",
+    "Referer": "https://item.jd.com/5177847.html",
     "Sec-Fetch-Mode": "no-cors",
     "User-Agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_0) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/77.0.3865.90 Safari/537.36"
 }
@@ -35,15 +35,15 @@ f.write("\n\n\n")
 # 每次爬取10条，共10000 条
 # 该商品只有1000条有效评论
 comment_count = 0
-for page in tqdm(range(0, 4300)):
+for page in tqdm(range(0, 5300)):
     # 当前爬取第 page 页
     query_args["page"] = str(page)
 
     # 获取响应信息
     res = tools.plugin_homepage(ip, 10, query_args, request_headers)
     if res['code'] != 200:
-        print 'Http Error'
-        exit(0)
+        print 'Skip'
+        continue
 
     # 移除首尾无用字符
     content = res['rsp_body']
@@ -57,7 +57,11 @@ for page in tqdm(range(0, 4300)):
     # 获取comments
     try:
         comments = data["comments"]
+        if len(comments) == 0:
+            print 'End in %s' % str(query_args["page"])
+            break
     except:
+        print 'End in %s' % str(query_args["page"])
         break
     for i in range(len(comments)):  # 循环遍历所有comments
         comment = comments[i]  # 单条comment
